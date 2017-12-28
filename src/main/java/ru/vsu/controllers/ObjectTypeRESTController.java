e93e8f5b8ba7879bd2dbf60cf5cfecaf5b10edcf
AleReTan <reshetnikov.alex93@gmail.com>
2017-12-28 23:31:52 +0300

    add /put

--- src/main/java/ru/vsu/controllers/ObjectTypeRESTController.java
+++ src/main/java/ru/vsu/controllers/ObjectTypeRESTController.java
@@ -1,6 +1,7 @@
 package ru.vsu.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,10 @@ public class ObjectTypeRESTController {
     @RequestMapping(value = "/ot",method = RequestMethod.GET)
     public List getOT(){
         return objectTypeService.getAll();
+    }
+
+    @RequestMapping(value = "ot/c", method = RequestMethod.PUT)
+    public void setOT(@RequestBody String ot){
+        System.out.println(ot);
     }
 }
