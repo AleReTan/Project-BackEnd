7f8b06a0fe6b3edd9235c852401c0fa46139224b
AleReTan <reshetnikov.alex93@gmail.com>
2018-02-15 14:09:07 +0300

    ±rework all that linked with params

--- src/main/java/ru/vsu/dao/daoImpl/ObjectDao.java
+++ src/main/java/ru/vsu/dao/daoImpl/ObjectDao.java
@@ -74,7 +74,8 @@ public class ObjectDao<T extends ObjectEntity> implements Dao<ObjectEntity> {
     }
 
     public List<Long> getListOfObjectIdByObjectTypeId(long objectTypeId) {
-        return jdbcTemplate.queryForList(GET_LIST_OF_OBJECT_ID, Long.class, objectTypeId);
+        String sql = "SELECT eav.object.id FROM  eav.object WHERE eav.object.object_type_id = ?";
+        return jdbcTemplate.queryForList(sql, Long.class, objectTypeId);
 
     }
 /*
