d12fc78d764643144212a2853a624ac4421ccfd2
AleReTan <reshetnikov.alex93@gmail.com>
2018-02-15 14:24:25 +0300

    ±rework sql string to constant string

--- src/main/java/ru/vsu/dao/daoImpl/ReferenceDao.java
+++ src/main/java/ru/vsu/dao/daoImpl/ReferenceDao.java
@@ -15,6 +15,16 @@ import java.util.Map;
 public class ReferenceDao implements Dao<ReferenceEntity> {
     private final JdbcTemplate jdbcTemplate;
 
+    private static final String DELETE = "DELETE  FROM  eav.reference WHERE eav.reference.object_id = ? AND eav.reference.attr_id=?";
+    private static final String DELETE_BY_OBJECT_ID = "DELETE  FROM  eav.reference WHERE eav.reference.object_id = ?";
+    private static final String INSERT = "INSERT INTO  eav.reference VALUES (? ,?, ?)";
+    private static final String UPDATE = "UPDATE eav.reference SET reference = ? WHERE eav.reference.object_id = ? AND eav.reference.attr_id = ?";
+    private static final String GET_ALL = "SELECT * FROM  eav.reference";
+    private static final String GET_BY_OBJECT_ID = "SELECT * FROM  eav.reference WHERE eav.reference.object_id = ?";
+    private static final String GET_BY_REFERENCE = "SELECT * FROM  eav.reference WHERE eav.reference.reference = ?";
+    private static final String GET_BY_REFERENCE_AND_OBJECT_ID = "SELECT * FROM  eav.reference WHERE eav.reference.reference = ? AND eav.reference.object_id";
+    private static final String GET_MAP_BY_OBJECT_ID = "SELECT * FROM  eav.reference r WHERE r.object_id = ?";
+
     @Autowired
     public ReferenceDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -22,65 +32,50 @@ public class ReferenceDao implements Dao<ReferenceEntity> {
 
     @Override
     public void delete(ReferenceEntity obj) {
-        String sql = "DELETE  FROM  eav.reference WHERE eav.reference.object_id = ? AND eav.reference.attr_id=?";
-        jdbcTemplate.update(sql, obj.getObjectId(), obj.getAttrId());
+        jdbcTemplate.update(DELETE, obj.getObjectId(), obj.getAttrId());
     }
 
     public void deleteByObjectId(long objectId) {
-        String sql = "DELETE  FROM  eav.reference WHERE eav.reference.object_id = ?";
-        jdbcTemplate.update(sql, objectId);
+        jdbcTemplate.update(DELETE_BY_OBJECT_ID, objectId);
     }
 
     @Override
     public void insert(ReferenceEntity obj) {
-        String sql = " INSERT INTO  eav.reference VALUES (? ,?, ?)";
-        jdbcTemplate.update(sql, obj.getReference(), obj.getObjectId(), obj.getAttrId());
+        jdbcTemplate.update(INSERT, obj.getReference(), obj.getObjectId(), obj.getAttrId());
     }
 
     public void insert(long reference, long objectId, long attributeId) {
-        String sql = " INSERT INTO  eav.reference VALUES (? ,?, ?)";
-        jdbcTemplate.update(sql, reference, objectId, attributeId);
+        jdbcTemplate.update(INSERT, reference, objectId, attributeId);
     }
 
     @Override
     public void update(ReferenceEntity obj) {
-        String sql = " UPDATE eav.reference SET reference = ? WHERE eav.reference.object_id = ? AND eav.reference.attr_id = ?";
-        jdbcTemplate.update(sql, obj.getReference(), obj.getObjectId(), obj.getAttrId());
+        jdbcTemplate.update(UPDATE, obj.getReference(), obj.getObjectId(), obj.getAttrId());
     }
 
     public void update(long newReference, long objectId, long attributeId) {
-        String sql = " UPDATE eav.reference SET reference = ? WHERE eav.reference.object_id = ? AND eav.reference.attr_id = ?";
-        jdbcTemplate.update(sql, newReference, objectId, attributeId);
+        jdbcTemplate.update(UPDATE, newReference, objectId, attributeId);
     }
 
     @Override
     public List<ReferenceEntity> getAll() {
-        String sql = "SELECT * FROM  eav.reference";
-        List<ReferenceEntity> list = jdbcTemplate.query(sql, new ReferenceMapper());
-        return list;
+        return jdbcTemplate.query(GET_ALL, new ReferenceMapper());
     }
 
     public List<ReferenceEntity> getReferencesByObjectId(long objectId) {
-        String sql = "SELECT * FROM  eav.reference WHERE eav.reference.object_id = ?";
-        List<ReferenceEntity> list = jdbcTemplate.query(sql, new ReferenceMapper(), objectId);
-        return list;
+        return jdbcTemplate.query(GET_BY_OBJECT_ID, new ReferenceMapper(), objectId);
     }
 
     public List<ReferenceEntity> getReferencesByRefId(long refId) {
-        String sql = "SELECT * FROM  eav.reference WHERE eav.reference.reference = ?";
-        List<ReferenceEntity> list = jdbcTemplate.query(sql, new ReferenceMapper(), refId);
-        return list;
+        return jdbcTemplate.query(GET_BY_REFERENCE, new ReferenceMapper(), refId);
     }
 
     public ReferenceEntity getReferencesByRefIdAndObjectId(long refId, long objectId) {
-        String sql = "SELECT * FROM  eav.reference WHERE eav.reference.reference = ? AND eav.reference.object_id";
-        ReferenceEntity referenceEntity = jdbcTemplate.queryForObject(sql, new ReferenceMapper(), refId, objectId);
-        return referenceEntity;
+        return jdbcTemplate.queryForObject(GET_BY_REFERENCE_AND_OBJECT_ID, new ReferenceMapper(), refId, objectId);
     }
 
     public Map<Long, Long> getReferenceMapByObjectId(long objectId) {
-        String sql = "SELECT * FROM  eav.reference r WHERE r.object_id = ?";
-        List<ReferenceEntity> list = jdbcTemplate.query(sql, new ReferenceMapper(), objectId);
+        List<ReferenceEntity> list = jdbcTemplate.query(GET_MAP_BY_OBJECT_ID, new ReferenceMapper(), objectId);
         Map<Long, Long> returnMap = new HashMap<>();
         for (ReferenceEntity referenceEntity : list) {
             returnMap.put(referenceEntity.getAttrId(), referenceEntity.getReference());
