2f5af9640232fa4fd695cd045389db9843b5e3d3
Testov <testovboriska@gmail.com>
2018-04-29 16:09:37 +0300

    для деплоя

--- src/main/java/ru/vsu/config/ServiceConfig.java
+++ src/main/java/ru/vsu/config/ServiceConfig.java
@@ -4,22 +4,29 @@ import org.postgresql.ds.PGPoolingDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "ru.vsu")
+@PropertySource("classpath:db.properties")
 public class ServiceConfig {
+
+    @Resource
+    private Environment env;
 
     @Bean
     public DataSource getDataSource() {
         @SuppressWarnings("deprecation")
         PGPoolingDataSource source = new PGPoolingDataSource();
-        source.setServerName("localhost");
-        source.setDatabaseName("postgres");
-        source.setUser("postgres");
-        source.setPassword("1488");
+        source.setServerName(env.getRequiredProperty("db.url"));
+        source.setDatabaseName(env.getRequiredProperty("db.name"));
+        source.setUser(env.getRequiredProperty("db.username"));
+        source.setPassword(env.getRequiredProperty("db.password"));
         source.setMaxConnections(10);
         return source;
     }
