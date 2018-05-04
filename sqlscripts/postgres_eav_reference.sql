e972a660ca8ec664f4646f72f29056245563562e
Alex Reshetnikov <reshetnikov.alex93@gmail.com>
2018-05-04 13:10:46 +0300

    fix

--- sqlscripts/postgres_eav_reference.sql
+++ sqlscripts/postgres_eav_reference.sql
@@ -1,5 +1,4 @@
 INSERT INTO eav.reference (reference, object_id, attr_id) VALUES (2, 27, 16);
 INSERT INTO eav.reference (reference, object_id, attr_id) VALUES (24, 5, 16);
-INSERT INTO eav.reference (reference, object_id, attr_id) VALUES (5, 260, 18);
-INSERT INTO eav.reference (reference, object_id, attr_id) VALUES (36, 37, 16);
 INSERT INTO eav.reference (reference, object_id, attr_id) VALUES (6, 12, 16);
+INSERT INTO eav.reference (reference, object_id, attr_id) VALUES (36, 37, 16);
